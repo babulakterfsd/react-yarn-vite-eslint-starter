f1e47d5ebcb8bc99e1c54674381d82b1633aeef3
Babul Akter <babulakterfsd@gmail.com>
2023-01-14 13:53:45 +0600

    all the routes and layouts have been created successfully..

--- src/components/dashboard/Balance.jsx
+++ src/components/dashboard/Balance.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Balance() {
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        // fetch balance from server
+        setTimeout(() => {
+            setBalance(7000);
+        }, 2000);
+    }, []);
+
+    if (balance === 0) {
+        return (
+            <div>
+                <p>Getting Balance from database...</p>
+            </div>
+        );
+    }
+
     return (
         <div>
-            <p>Your balance is $3000</p>
+            <p>Your balance is {`${balance}`}</p>
         </div>
     );
 }
